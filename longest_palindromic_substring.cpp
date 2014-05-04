1c0c4a40810251abc78a176153565dfee623b540
vwagen <sean_wx@foxmail.com>
2014-05-04 14:52:05 +0800

    resolved longest palindrmic substring

---
+++ longest_palindromic_substring.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+private:
+    typedef struct {
+        int pos;
+        int length;
+    } index_pair;
+public:
+    string longestPalindrome(string s) {
+        int len = s.size();
+        if(len < 3) {
+            return s;
+        }
+        int head;
+        int tail;
+        int head_tmp;
+        head = 2;
+        vector<index_pair> rets;
+        while(head < len - 1) {
+            tail = head + 1;
+            head_tmp = head;
+            if(s[head] != s[tail] && s[head - 1] != s[tail]) {
+                head = head_tmp + 1;
+                continue;
+            }
+            if(s[head] == s[tail]) {
+                while(s[head] == s[tail] && head >= 0 && tail < len) {
+                    head--;
+                    tail++;
+                }
+                index_pair ret_tmp = {head + 1, tail - head - 1};
+                rets.push_back(ret_tmp);
+            }
+            head = head_tmp;
+            if(s[head - 1] == s[tail]) {
+                head--;
+                while(s[head] == s[tail] && head >= 0 && tail < len) {
+                    head--;
+                    tail++;
+                }
+                index_pair ret_tmp = {head + 1, tail - head - 1};
+                rets.push_back(ret_tmp);
+            }
+            head = head_tmp + 1;
+        }
+        vector<index_pair>::iterator it_max;
+        vector<index_pair>::iterator it = rets.begin();
+        int max_length = 0;
+        while(it != rets.end()) {
+            cout << (*it).pos << "    " << (*it).length << endl;
+            if(max_length < (*it).length) {
+                cout << "max_length " << max_length << " less than (*it).length " << (*it).length << endl;
+                max_length = (*it).length;
+                it_max = it;
+            }
+            it++;
+        }
+        return s.substr((*it_max).pos, (*it_max).length);
+    }
+};
+
+int main(int argc, char* argv[]) {
+    string s;
+    if(argc == 2) {
+        string s_tmp(argv[1]);
+        s = s_tmp;
+    }
+    else {
+        string s_tmp("kabcdcbam");
+        s = s_tmp;
+    }
+    Solution sol;
+    cout << sol.longestPalindrome(s) << endl;
+    return 0;
+}
